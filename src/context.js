05bbd0a4f2b83fb90336f8692e4a635815e3c46b
Guna Shekar <gunashekherproddatoori@gmail.com>
2019-10-08 23:59:49 +0530

    Setup Context

---
+++ src/context.js
@@ -0,0 +1,29 @@
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+const secret = process.env.JWT_TOKEN;
+
+const provideContext = (request) => {
+	const req = request.req;
+	var payload = {
+		isValid: false,
+		token: null,
+	};
+	let authHeader = req.headers.authorization || null;
+
+	if(authHeader){
+		const token = authHeader.replace('bearer ','');
+
+		jwt.verify(token, secret, (err, decoded) => {
+			if(!err){
+        payload.isValid = true;
+        payload.token = token;
+        payload.email = decoded.email;
+        payload.id = decoded.id;
+			}
+		});
+	}
+  return payload;
+}
+
+module.exports = provideContext;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const cors = require('cors');
+const apolloServer = require('apollo-server-express');
+
+const typeDefs = require('./schema');
+const resolvers = require('./resolvers');
+const provideContext = require('./context');
+
 require('dotenv').config();
 
 const app = express();
 app.use(cors());
+
+const server = new apolloServer({
+	typeDefs,
+	resolvers,
+	context: (req) => provideContext(req);
+});
 
 const port = process.env.PORT || 3001;
 
