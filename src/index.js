976363cda248cad47f4f2bb692e9b81f3cc050f6
Prem Kumar <prmsrswt@gmail.com>
2019-10-09 18:18:36 +0530

    set up mongodb, global config

--- src/index.js
+++ src/index.js
@@ -1,29 +1,47 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient } = require('mongodb');
 const { ApolloServer } = require('apollo-server-express');
 
 const typeDefs = require('./schema');
 const resolvers = require('./resolvers');
 const provideContext = require('./context');
 
-require('dotenv').config();
+const config = require('./utils/config');
 
 const app = express();
 app.use(cors());
 
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  context: req => provideContext(req),
-});
+MongoClient.connect(config.dbHost, {
+  auth: {
+    user: config.dbUser,
+    password: config.dbPass,
+    authSource: config.dbName,
+  },
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(client => {
+    const db = client.db(config.dbName);
 
-const port = process.env.PORT || 3001;
+    const server = new ApolloServer({
+      typeDefs,
+      resolvers,
+      context: req => provideContext(req, db),
+    });
 
-app.get('/api', (req, res) => res.send('it is working.'));
+    const port = config.port || 3001;
 
-server.applyMiddleware({ app, path: '/api/graphql' });
+    app.get('/api', (_req, res) => res.send('it is working.'));
 
-app.listen(port, () =>
-  // eslint-disable-next-line no-console
-  console.log(`API server ready at http://localhost:${port}${server.graphqlPath}`)
-);
+    server.applyMiddleware({ app, path: '/api/graphql' });
+
+    app.listen(port, () =>
+      // eslint-disable-next-line no-console
+      console.log(`API server ready at http://localhost:${port}${server.graphqlPath}`)
+    );
+  })
+  .catch(err => {
+    // eslint-disable-next-line no-console
+    console.log('I caught something', err);
+  });
