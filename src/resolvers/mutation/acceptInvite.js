6004f55baa69cf63cb585c727c91f91f8b44eebd
Katakam Mallikarjun <mallik813@gmail.com>
2019-11-05 18:22:40 +0530

    format acceptInvite.js, change for loop to promise.all

--- src/resolvers/mutation/acceptInvite.js
+++ src/resolvers/mutation/acceptInvite.js
@@ -2,26 +2,33 @@ const { ApolloError, AuthenticationError } = require('apollo-server-express');
 
 const acceptInvite = async (_, args, context) => {
   const { isValid, db, client, id } = context;
+
   if (isValid) {
     const { teamId } = args;
     const team = await db
       .collection('teams')
       .find({ _id: teamId, pendingInvitations: id })
       .toArray();
+
     if (team.length === 0) throw new ApolloError('Invalid Team');
+
     const user = await db
       .collection('users')
       .find({ _id: id, 'teamInvitations.teamId': teamId })
       .toArray();
+
     if (user.length === 0) throw new ApolloError('You are not invited');
+
     const invites = await db
       .collection('users')
       .aggregate([
+        { $match: { _id: id } },
         { $unwind: '$teamInvitations' },
         { $match: { 'teamInvitations.eventId': team[0].event } },
         { $project: { 'teamInvitations.teamId': 1 } },
       ])
       .toArray();
+
     const session = client.startSession({
       defaultTransactionOptions: {
         readConcern: { level: 'local' },
@@ -29,11 +36,13 @@ const acceptInvite = async (_, args, context) => {
         readPreference: 'primary',
       },
     });
+
     try {
       await session.withTransaction(async () => {
         const usersCollection = db.collection('users');
         const teamsCollection = db.collection('teams');
-        await usersCollection.updateOne(
+
+        const userRes = usersCollection.updateOne(
           { _id: id },
           {
             $push: { teams: { teamId, eventId: team[0].event } },
@@ -41,21 +50,21 @@ const acceptInvite = async (_, args, context) => {
           },
           { session }
         );
-        await teamsCollection.updateOne(
+        const teamRes = teamsCollection.updateOne(
           { _id: teamId },
           {
             $push: { members: id },
           },
           { session }
         );
-        for (let i = 0; i < invites.length; i += 1) {
-          // eslint-disable-next-line no-await-in-loop
-          await teamsCollection.updateOne(
-            { _id: invites[i].teamInvitations.teamId },
+        const invitePromises = invites.map(invite => {
+          return teamsCollection.updateOne(
+            { _id: invite.teamInvitations.teamId },
             { $pull: { pendingInvitations: id } },
             { session }
           );
-        }
+        });
+        return Promise.all(invitePromises.concat([userRes, teamRes]));
       });
     } catch (err) {
       throw new ApolloError('Something went wrong', 'TRX_FAILED');
