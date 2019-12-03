84874774db797302c952dee20de55886d5d00f2c
Katakam Mallikarjun <mallik813@gmail.com>
2019-12-04 04:56:47 +0530

    jwt-sub in verifyForgotPwd.js

--- src/resolvers/mutation/verifyForgotPwd.js
+++ src/resolvers/mutation/verifyForgotPwd.js
@@ -1,11 +1,11 @@
 const { ApolloError } = require('apollo-server-express');
 const jwt = require('jsonwebtoken');
-const { jwtSecret } = require('../../utils/config');
+const { jwtHsSecret } = require('../../utils/config');
 
 const verifyForgotPwd = async (_, args) => {
   const { token } = args;
-  jwt.verify(token, jwtSecret.pubKey, { algorithm: 'ES512' }, err => {
-    if (err) throw new ApolloError(err, 'JWT_ERROR');
+  jwt.verify(token, jwtHsSecret, (err, decoded) => {
+    if (!err && decoded.sub !== 'ForgotPassword') throw new ApolloError(err, 'there was an error');
   });
   return token;
 };
