f16093cd27c71ec0c2e6a9e00fbab7c83782801a
Prem Kumar <prmsrswt@gmail.com>
2020-01-15 12:44:13 +0530

    check for xss during profile update

--- src/resolvers/mutation/updateProfile.js
+++ src/resolvers/mutation/updateProfile.js
@@ -1,14 +1,28 @@
 const { ApolloError, AuthenticationError } = require('apollo-server-express');
+const xss = require('xss');
 
 const updateProfile = async (_, args, context) => {
   const { isValid, db, client, id, logger } = context;
-  const { name, college, city, phone } = args;
+
+  const xssOptions = {
+    whiteList: [],
+    stripIgnoreTag: [],
+    stripIgnoreTagBody: ['script'],
+  };
+
+  const name = xss(args.name, xssOptions);
+  const college = xss(args.college, xssOptions);
+  const city = xss(args.city, xssOptions);
+  const phone = xss(args.phone, xssOptions);
+
   if (isValid) {
     if (!name || !college || !city || !phone)
       throw new ApolloError('Enter all fields', 'FIELDS_EMPTY');
+
     const regex = /^[6-9]\d{9}$/;
     const res = regex.test(phone);
     if (!res) throw new ApolloError('Enter valid phone number', 'PHONE_INVALID');
+
     const session = client.startSession({
       defaultTransactionOptions: {
         readConcern: { level: 'local' },
@@ -25,18 +39,19 @@ const updateProfile = async (_, args, context) => {
           { $set: { name, college, phone, city } },
           { session }
         );
-        return Promise.all([userRes]);
+        return userRes;
       });
     } catch (err) {
       logger('[TRX_ERR]', err);
       throw new ApolloError('Something went wrong', 'TRX_FAILED');
     } finally {
+      logger('[PROFILE_UPDATE]', 'user =>', id);
       await session.endSession();
     }
     return {
       code: 200,
       success: true,
-      message: 'Invite declined',
+      message: 'Profile updated successfully',
       user: { id },
     };
   }
